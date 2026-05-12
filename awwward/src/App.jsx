864cbeec7b8b6631adbfd99041921d283cc0e142
Kunal Deshmukh <deshmukh1207@gmail.com>
2026-05-12 22:30:07 +0530

    features file added

--- awwward/src/App.jsx
+++ awwward/src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Navbar from './components/Navbar'
 import About from './components/About'
 import Hero from './components/Hero'
+import Features from './components/Features'
 
 
 
@@ -11,6 +12,7 @@ const App = () => {
       <Navbar/>
       <Hero/>
       <About/>
+      <Features/>
     </main>
   )
 }
